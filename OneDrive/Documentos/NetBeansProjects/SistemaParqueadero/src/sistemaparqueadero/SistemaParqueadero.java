9c643ca387ed90a09907dae0cdb9f044e9537b89
Hjhonatan <jhonatandavidherrerarojas@gmail.com>
2025-01-29 11:21:52 -0500

    Creacion de los javaPackages

--- OneDrive/Documentos/NetBeansProjects/SistemaParqueadero/src/sistemaparqueadero/SistemaParqueadero.java
+++ OneDrive/Documentos/NetBeansProjects/SistemaParqueadero/src/sistemaparqueadero/SistemaParqueadero.java
@@ -1,19 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package sistemaparqueadero;
 
-/**
- *
- * @author jhona
- */
+
 public class SistemaParqueadero {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
-        System.out.println("Commit de prueba");    }
+        System.out.println("Creacion paquetes");        
     
+}
 }
