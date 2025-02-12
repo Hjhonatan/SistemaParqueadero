420ff84b096f1e68227048805ad1bd151e429a4d
Hjhonatan <jhonatandavidherrerarojas@gmail.com>
2025-02-11 21:20:04 -0500

    Se realiza login primera parte

--- OneDrive/Documentos/NetBeansProjects/SistemaParqueadero/src/sistemaparqueadero/SistemaParqueadero.java
+++ OneDrive/Documentos/NetBeansProjects/SistemaParqueadero/src/sistemaparqueadero/SistemaParqueadero.java
@@ -7,9 +7,6 @@ public class SistemaParqueadero {
   
     public static void main(String[] args) {
         System.out.println("Creacion paquetes");       
-        System.out.println("Prueba Pull");    
-        System.out.println("PRUEBA HOY PUSH");
-  
-        System.out.println("comit alejandro");
+
 }
 }
